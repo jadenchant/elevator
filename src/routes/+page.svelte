fa8eca64195fcee38df7d36e4a37ab27c911e37a
Jaden Chant <chant1@stolaf.edu>
2024-11-03 18:01:28 -0600

    added elevator doors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,13 @@
 	import { browser } from '$app/environment';
 	import * as THREE from 'three';
 
+	const clock = new THREE.Clock();
+
 	let camera: THREE.PerspectiveCamera;
 	let scene: THREE.Scene;
 	let renderer: THREE.WebGLRenderer;
-	let cube: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
+	let doorR: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
+	let doorL: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
 	let planeR: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
 	let planeL: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
 	let planeT: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
@@ -20,11 +23,15 @@
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		document.body.appendChild(renderer.domElement);
 
-		const boxgeo = new THREE.BoxGeometry(1, 2, 1);
-		const boxmat = new THREE.MeshBasicMaterial({ color: 0xffffff });
-		cube = new THREE.Mesh(boxgeo, boxmat);
-		cube.position.y = 1;
-		scene.add(cube);
+		const doorgeo = new THREE.BoxGeometry(0.5, 2, 0.05);
+		const doormat = new THREE.MeshBasicMaterial({ color: 0xffffff });
+		doorR = new THREE.Mesh(doorgeo, doormat);
+		doorR.position.set(0.25, 1, 0.25);
+		scene.add(doorR);
+
+		doorL = new THREE.Mesh(doorgeo, doormat);
+		doorL.position.set(-0.25, 1, 0.25);
+		scene.add(doorL);
 
 		const planegeo = new THREE.PlaneGeometry(4, 2);
 		const planemat = new THREE.MeshBasicMaterial({ color: 0xdddddd });
@@ -36,9 +43,9 @@
 		planeL.position.set(-2.5, 1, 0.5);
 		scene.add(planeL);
 
-		const planegeoT = new THREE.PlaneGeometry(9, 0.25);
+		const planegeoT = new THREE.PlaneGeometry(9, 0.4);
 		planeT = new THREE.Mesh(planegeoT, planemat);
-		planeT.position.set(0, 2.125, 0.5);
+		planeT.position.set(0, 2.2, 0.5);
 		scene.add(planeT);
 
 		// Temp Grid Helper
@@ -56,9 +63,17 @@
 	const animateScene = () => {
 		animationFrameId = requestAnimationFrame(animateScene);
 
+		const delta = clock.getDelta();
+		const sec = clock.getElapsedTime();
+
 		// if (camera.position.z > 2) {
 		// 	camera.position.z -= 0.005;
 		// }
+
+		if (sec > 2 && doorL.position.x > -0.75) {
+			doorL.position.x -= 0.005;
+			doorR.position.x += 0.005;
+		}
 
 		renderScene();
 	};
